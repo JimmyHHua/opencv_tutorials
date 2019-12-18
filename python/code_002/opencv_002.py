9b7777307d8fdaa776916ab6b4c6f01fde8a8451
JimmyHHua <550452087@qq.com>
2019-12-18 16:42:38 +0800

    modify the code error

--- python/code_002/opencv_002.py
+++ python/code_002/opencv_002.py
@@ -3,7 +3,7 @@ import cv2
 src = cv2.imread("./test.png")
 cv2.namedWindow("input", cv2.WINDOW_AUTOSIZE)
 cv2.imshow("input", src)
-gray = cv2.cv2tColor(src, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 cv2.imwrite('gray.png', gray)
 cv2.imshow("gray", gray)
 cv2.waitKey(0)
